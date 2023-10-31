bb60cfcff7729fdc53e780546d1b5a8a1293603e
matvey <matveyvlasov364@gmail.com>
2023-11-01 00:40:46 +0300

    Fixed

--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,34 @@
+from src.lab1.calculator import calc
+import sys
+# sys.path.append(r"Users/matve/OneDrive/Desktop/cs102")
 import unittest
+
 
 class CalculatorTestCase(unittest.TestCase):
 
-    # Тест для проверки работы, можно удалить
     def test_one(self):
-        self.assertEquals(1, 1)
+        op = '100-50'
+        cr = 50
+        res = calc(op)
+        self.assertEqual(cr, res)
+
+    def test_two(self):
+        op = "privet"
+        cr = 'Неверный ввод. Пишите слитно, используя существующие числа и знаки операций'
+        res = calc(op)
+        self.assertEqual(cr, res)
+
+    def test_three(self):
+        op = "2 + 2"
+        cr = 'Неверный ввод. Пишите слитно, используя существующие числа и знаки операций'
+        res = calc(op)
+        self.assertEqual(cr, res)
+
+    def test_four(self):
+        op = "2/0"
+        cr = 'Делить на ноль нельзя'
+        res = calc(op)
+        self.assertEqual(cr, res)
+
+if __name__ == '__main__':
+    unittest.main()
